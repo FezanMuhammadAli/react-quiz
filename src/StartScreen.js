6cbed2f798b70210cdf5d88769ce467dee484404
Fezan Muhmmad ALi <callmetheengcountio@gmail.com>
2023-10-28 00:43:08 +0500

    created StartScreen.js to display when the state is in 'ready'

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button className="btn btn-ui">Let's Start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
