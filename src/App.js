c122b6c44092ecab6954a4fb9a531d6f05005b2d
Fezan Muhmmad ALi <callmetheengcountio@gmail.com>
2023-10-28 00:30:14 +0500

    managing loading, questions state via useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+const initialState = {
+  questions: [],
+  // loading,error,ready,active,finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("action unknown");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
