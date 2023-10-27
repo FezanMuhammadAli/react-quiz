f87752dd09b6658d8aefc3692580b1ff41ce72ae
Fezan Muhmmad ALi <callmetheengcountio@gmail.com>
2023-10-28 00:50:48 +0500

    updated for 'active' status that will use the Question component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -18,6 +19,9 @@ function reducer(state, action) {
 
     case "dataFailed":
       return { ...state, status: "error" };
+
+    case "start":
+      return { ...state, status: "active" };
 
     default:
       throw new Error("action unknown");
@@ -41,7 +45,10 @@ function App() {
       <Main className="main">
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
