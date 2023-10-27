ddaa77949e56a60ad1794a79fb4488aba31c482a
Fezan Muhmmad ALi <callmetheengcountio@gmail.com>
2023-10-28 01:48:37 +0500

    via prop drilling dispatcher is used to check the answer is right or not

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,10 +1,27 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div>
       <div className="options">
-        {question.options.map((option) => {
+        {question.options.map((option, index) => {
           return (
-            <button className="btn btn-option" key={option}>
+            <button
+              className={`btn btn-option ${index === answer ? "answer" : ""} ${
+                hasAnswered
+                  ? index === question.correctOption
+                    ? "correct"
+                    : "wrong"
+                  : ""
+              }`}
+              key={option}
+              disabled={answer !== null}
+              onClick={() =>
+                dispatch({
+                  type: "newAnswer",
+                  payload: index,
+                })
+              }
+            >
               {option}
               {console.log("option", option)}
             </button>
