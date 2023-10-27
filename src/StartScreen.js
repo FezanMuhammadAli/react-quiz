b3f6370e640e8eae7307b2da27579ed2fc32755b
Fezan Muhmmad ALi <callmetheengcountio@gmail.com>
2023-10-28 00:49:31 +0500

    just updated to change the state of status to 'active'

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,18 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui">Let's Start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() =>
+          dispatch({
+            type: "start",
+          })
+        }
+      >
+        Let's Start
+      </button>
     </div>
   );
 }
