43c76e29ec46ae0ac84f8e52b27aec9f3eff2ac7
Fezan Muhmmad ALi <callmetheengcountio@gmail.com>
2023-10-29 08:25:06 +0500

    there was a bug to read the properties of correctOption so was testing purposes it was checked. is it giving true or not

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -3,6 +3,7 @@ import Options from "./Options";
 function Question({ question, dispatch, answer }) {
   return (
     <div>
+      {/* {console.log("question", question)} */}
       <h4>{question.question}</h4>
       <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
